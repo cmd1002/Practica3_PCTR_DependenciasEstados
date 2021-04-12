bcb9050a04c7c53ab29a39d2d2b0dbd154cc58ec
Carlos Manso Díez <Carlos@DESKTOP-1NJF7P6>
2021-04-12 09:33:50 +0200

    ActividadSalida

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,32 @@
 package src.p03.c01;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ActividadSalidaPuerta{
 
-	//
-	// TODO
-	//
+	private int personas;
+	private String puerta;
+	private IParque parque;
+
+	public ActividadSalidaPuerta(String puerta, IParque parque, int personas) {
+		this.puerta = puerta;
+		this.parque = parque;
+		this.personas = personas;
+	}
+
+	public void run() {
+		for (int i = personas; i > 0; i --) {
+			try {
+				parque.entrarAlParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
+	}
 }
